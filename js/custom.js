8fc63b03c357b1337883fe50dc5c6b408193b022
Jiyapatidar0812 <Jiyapatidar0812@gmail.com>
2023-06-23 23:31:04 +0530

    new

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+
+//function definition
+function getMyFullName(...args){
+    console.log(args);
+}
+
+//2.Function calling/invoking
+getMyFullName("Jiya",'patidar',20,5.2,["garvit","diyara"]);
+
---
+++ js/script.js
@@ -0,0 +1,19 @@
+const x = ['Jiya'];
+
+const y = [...x,"patidar"]; // ...x is spread operator
+
+console.log(y);
+
+let n1 =["Congress"];
+let news =[...n1,'president'];
+let news2=[...news," Mallikarjun Kharge"];
+let news3=[...news2,"party leader"];
+let n4=[...news3,'Rahul Gandhi,'];
+let news5=[...n4,"West Bengal"];
+let n6=[...news5,"Chief Minister"];
+
+
+console.log(n6);
+
+
+
